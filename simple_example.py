ecbbaf750887291f4bddffb5f4a8d701a243fd80
gukush <s184339@student.pg.edu.pl>
2024-10-23 22:18:06 +0200

    Added some temporary profiling code

--- simple_example.py
+++ simple_example.py
@@ -16,7 +16,10 @@ from openvoice.api import ToneColorConverter
 import silentcipher
 import librosa
 import soundfile
-
+import psutil
+import gc
+#import torch.profiler as profiler
+import tracemalloc
 # adapted from following collab notebook: https://colab.research.google.com/github/facebookresearch/audioseal/blob/master/examples/colab.ipynb#scrollTo=007c48cb
 # and this jupyter notebook: https://github.com/myshell-ai/OpenVoice/blob/main/demo_part3.ipynb
 
@@ -55,6 +58,8 @@ def voice_cloning():
                                  tgt_se=target_se,
                                  output_path='audio/voice-hispanic-converted.wav',
                                  message=encode_message) #they use wavmark for watermarking
+    del tone_color_converter
+    gc.collect()
 
 def download_audio(url,output):
     if not os.path.isfile(output):
@@ -79,6 +84,8 @@ def audio_watermarking():
     # TODO: handle paths in a better fashion
     subprocess.call(['python','NISQA/run_predict.py','--mode','predict_file','--pretrained_model',
                      '/project/NISQA/weights/nisqa.tar','--deg',watermark_path,'--output_dir','/project/results'])
+    del model
+    gc.collect()
 
 def detecting_from_clone():
     input_path = "/project/audio/voice-hispanic-1-watermarked_mono.opus"
@@ -95,25 +102,55 @@ def detecting_from_clone():
     result, message = detector.detect_watermark(audio,sample_rate=sr,message_threshold=0.5)
     print(f"\n{input_path} audio has {result*100}% probability of being watermarked")
     print(message)
+    del detector
+    gc.collect()
 
+
+top_stats = None
 def test_silentcipher():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = silentcipher.get_model(model_type='44.1k',device=device)
+    snapshot = tracemalloc.take_snapshot()
+    top_stats = snapshot.statistics('lineno')
+    if device == 'cpu':
+        print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
+    else:
+        gpu_idx = torch.cuda.current_device()
+        print(f"Total Memory: {torch.cuda.get_device_properties(gpu_idx).total_memory / (1024 ** 2):.2f} MB")
+        print(f"Memory Allocated: {torch.cuda.memory_allocated(gpu_idx) / (1024 ** 2):.2f} MB")
+        print(f"Memory Cached: {torch.cuda.memory_reserved(gpu_idx) / (1024 ** 2):.2f} MB")
     audio, sr = librosa.load('/project/audio/voice-polish-1.wav')
     encoded, sdr = model.encode_wav(audio, sr, [123, 234, 111, 222, 11])
     soundfile.write("/project/audio/voice-polish-1-silentcipher.wav",encoded,sr,format='wav')
     result = model.decode_wav(encoded,sr,phase_shift_decoding=False)
     print(result)
+    del model
+    gc.collect()
 
+def trace_handler(prof):
+    print(prof.key_averages().table(
+        sort_by="self_cpu_memory_usage", row_limit=-1))
+    prof.export_chrome_trace("/project/tmp/test_trace_" + str(prof.step_num) + ".json")
 
 def main():
-    test_silentcipher()
-    audio_watermarking()
-    voice_cloning()
-    detecting_from_clone()
+    tracemalloc.start()
+    with torch.no_grad():
+        #with profiler.profile(
+        #    activities=[torch.profiler.ProfilerActivity.CPU],
+        #    with_stack=True,profile_memory=True) as prof:
+        test_silentcipher()
+        snapshot = tracemalloc.take_snapshot()
+        audio_watermarking()
+        snapshot = tracemalloc.take_snapshot()
+        voice_cloning()
+        snapshot = tracemalloc.take_snapshot()
+        detecting_from_clone()
+        snapshot = tracemalloc.take_snapshot()
+    #print(prof.key_averages().table(sort_by="self_cpu_memory_usage",row_limit=-1))
     #subprocess.call(['python','NISQA/run_predict.py','--mode','predict_file','--pretrained_model',
     #                 '/proj/NISQA/weights/nisqa.tar','--deg','/proj/'+output,'--output_dir','/proj/results'])
 
 
 if __name__ == '__main__':
     main()
+    print(top_stats)
