12e1d25982a16697a8668b3926c250a6168009f8
gukush <s184339@student.pg.edu.pl>
2024-11-07 20:31:59 +0100

    minor workarounds

--- tests/yourtts_test.py
+++ tests/yourtts_test.py
@@ -2,5 +2,6 @@ import torch
 from TTS.api import TTS
 
 # Example voice cloning with YourTTS in English, French and Portuguese
+device = 'cpu'
 tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False).to(device)
 tts.voice_conversion_to_file(ource_wav="/audio/voice-hispanic-1.wav", target_wav="/audio/voice-polish-8.wav", file_path="output.wav")
