088e498ce3c714c7c66ceb272b14b0dd007208fe
gukush <s184339@student.pg.edu.pl>
2024-11-08 11:47:16 +0100

    progressing with bark test

--- tests/bark_test.py
+++ tests/bark_test.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-submodule_path = os.path.join(os.path.dirname(__file__),'..')#,'bark_with_voice_clone')
-sys.path.append(submodule_path)
-
+root_path = os.path.join(os.path.dirname(__file__),'..')#,'bark_with_voice_clone')
+sys.path.append(root_path)
+submodule_path = os.path.join(root_path,'bark-with-voice-clone')
 from bark_with_voice_clone.bark.generation import load_codec_model, generate_text_semantic
 from encodec.utils import convert_audio
 
@@ -22,8 +22,8 @@ hubert_manager.make_sure_tokenizer_installed()
 from bark_with_voice_clone.hubert.pre_kmeans_hubert import CustomHubert
 from bark_with_voice_clone.hubert.customtokenizer import CustomTokenizer
 
-hubert_model = CustomHubert(checkpoint_path='/project/data/models/hubert/hubert.pt').to(device)
-tokenizer = CustomTokenizer(checkpoint_path='/project/data/models/hubert/hubert.pt').to(device)
+hubert_model = CustomHubert(checkpoint_path='/project/data/models/hubert/hubert.pt',device=device).to(device)
+tokenizer = CustomTokenizer.load_from_checkpoint('/project/data/models/hubert/tokenizer.pth',map_location=device).to(device)
 
 audio_filepath = '/project/audio/old/voice-polish-1.wav'
 audio, sr = torchaudio.load(audio_filepath)
@@ -40,9 +40,9 @@ codes = codes.cpu().numpy()
 
 semantic_tokens = semantic_tokens.cpu().numpy()
 import numpy as np
-voice_name = 'output'
+voice_name = 'polish_speaker_0'
 
-output_path = submodule_path+'bark/assets/prompts/' + voice_name + '.npz'
+output_path = os.path.join(submodule_path,'bark/assets/prompts/',voice_name + '.npz')
 np.savez(output_path, fine_prompt=codes, coarse_prompt=codes[:2, :], semantic_prompt=semantic_tokens)
 converted_path = '/project/audio/old/voice_cloned_bark.wav'
 
@@ -58,7 +58,10 @@ wav_2 = wav_2.to(device)
 
 semantic_vectors_2 = hubert_model.forward(wav_2,input_sample_hz=model.sample_rate)
 semantic_tokens_2 = tokenizer.get_token(semantic_vectors_2)
+semantic_tokens_2 = semantic_tokens_2.cpu().numpy()
 
 
-cloned_audio = semantic_to_waveform(semantic_tokens_2,history_prompt='output')
+#try loading models earlier?
+breakpoint()
+cloned_audio = semantic_to_waveform(semantic_tokens_2,history_prompt=voice_name)
 
